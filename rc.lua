25e4f7813097707d9d01bea1967383b501ea94d8
Miguel García Soler <eligum.djo@gmail.com>
2022-01-28 01:01:22 +0100

    Removed anoying default Run prompt

--- rc.lua
+++ rc.lua
@@ -541,8 +541,8 @@ globalkeys = gears.table.join(
               {description = "show the menubar", group = "launcher"}),
 
     -- Prompt
-    awful.key({ modkey }, "r", function () awful.screen.focused().mypromptbox:run() end,
-              {description = "run prompt", group = "launcher"}),
+    --awful.key({ modkey }, "r", function () awful.screen.focused().mypromptbox:run() end,
+    --          {description = "run prompt", group = "launcher"}),
 
     awful.key({ modkey }, "x",
               function ()
